192d969c3a5d490916e22e5f7bb2526afb35f634
Ian Sills <iansills04@gmail.com>
2023-10-05 21:00:30 -0500

    updated js

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
     console.log("Ready!");
 });
-    //Display current date and time (Moment.js)
+    
     let now = moment().format("dddd, MMMM Do YYYY");
     let displayDate = document.getElementById("currentDay");
     displayDate.innerHTML = now;
     let currentHour = moment().format("HH");
 
-    //Past, present, and future functionality -
-    //Compare timeblock time with current time to determine whether
-    //activity is in the past, present, or future 
     $(".time-div").each(function() {
         var timeDiv = $(this).attr("id").split("-")[1];
 
@@ -28,7 +25,7 @@ $(document).ready(function() {
     });
 
     
-    //Save data to local storage
+  
     $(".saveBtn").click(function (event) {
         event.preventDefault();
         var value = $(this).siblings(".time-block").val();
@@ -36,7 +33,7 @@ $(document).ready(function() {
         localStorage.setItem(time,value);
     });
     
-     //Retrieve data from local storage 
+     
      $("#hour-09 .time-block").val(localStorage.getItem("09"));
      $("#hour-10 .time-block").val(localStorage.getItem("10"));
      $("#hour-11 .time-block").val(localStorage.getItem("11"));
@@ -47,7 +44,6 @@ $(document).ready(function() {
      $("#hour-16 .time-block").val(localStorage.getItem("16"));
      $("#hour-17 .time-block").val(localStorage.getItem("17"));
       
-      //Clear button function for clearing content and local storage
       $("#clearFieldsBtn").click(function(event) {
         event.preventDefault;
         $("textArea").val("");
