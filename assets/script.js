ae218b2d71b29028796e9d9d8269292fdebf0f80
Ian Sills <iansills04@gmail.com>
2023-10-05 20:59:21 -0500

    updated js

--- assets/script.js
+++ assets/script.js
@@ -35,3 +35,15 @@ $(document).ready(function() {
         var time = $(this).parent().attr("id").split("-")[1];
         localStorage.setItem(time,value);
     });
+    
+     //Retrieve data from local storage 
+     $("#hour-09 .time-block").val(localStorage.getItem("09"));
+     $("#hour-10 .time-block").val(localStorage.getItem("10"));
+     $("#hour-11 .time-block").val(localStorage.getItem("11"));
+     $("#hour-12 .time-block").val(localStorage.getItem("12"));
+     $("#hour-13 .time-block").val(localStorage.getItem("13"));
+     $("#hour-14 .time-block").val(localStorage.getItem("14"));
+     $("#hour-15 .time-block").val(localStorage.getItem("15"));
+     $("#hour-16 .time-block").val(localStorage.getItem("16"));
+     $("#hour-17 .time-block").val(localStorage.getItem("17"));
+ 
