b6b6089681e542cacf73b174a832b074a0c4df20
Ian Sills <iansills04@gmail.com>
2023-10-05 20:57:41 -0500

    updated js

--- assets/script.js
+++ assets/script.js
@@ -7,4 +7,22 @@ $(document).ready(function() {
     displayDate.innerHTML = now;
     let currentHour = moment().format("HH");
 
-    
+    //Past, present, and future functionality -
+    //Compare timeblock time with current time to determine whether
+    //activity is in the past, present, or future 
+    $(".time-div").each(function() {
+        var timeDiv = $(this).attr("id").split("-")[1];
+
+        if (currentHour == timeDiv) {
+            $(this).addClass("present");
+            $(this).children(".description").addClass("present");
+   
+        } else if (currentHour < timeDiv) {
+            $(this).removeClass("present");
+            $(this).addClass("future");
+
+        } else if (currentHour > timeDiv) {
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+    });
